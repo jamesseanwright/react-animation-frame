16c0ffe274c6b572ea9f23c3c8519a9031594da2
James Wright <JAWright@Channel4.co.uk>
2017-03-10 12:01:19 +0000

    First test passing

--- src/AnimationFrameComponent.jsx
+++ src/AnimationFrameComponent.jsx
@@ -2,10 +2,25 @@
 
 const React = require('react');
 
-module.exports = function AnimationFrameComponent() {
+module.exports = function AnimationFrameComponent(InnerComponent) {
 	return class AnimatedComponent extends React.Component {
+		constructor() {
+			super();
+			this.loop = this.loop.bind(this);
+		}
+
+		loop(time) {
+			this.innerComponent.onAnimationFrame(time);
+			requestAnimationFrame(this.loop);
+		}
+
+		componentDidMount() {
+			requestAnimationFrame(this.loop);
+		}
+
 		render() {
-			return <div />;
+			return <InnerComponent ref={node => this.innerComponent = node}
+				{...this.props} />;
 		}
 	};
 };
